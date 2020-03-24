ba9de6b165ce8d4767e248ab81e3fa171437801e
Jose Luis Cahuana <joseluis.cahuana@hotmail.com>
2020-03-24 18:31:49 -0500

    Updates

---
+++ precache-manifest.f053a0601c678c16f18660a8aff7ab0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2882d260381f099043fbdd16351d9c03",
+    "url": "/imagenes/index.html"
+  },
+  {
+    "revision": "24b203d241c93b80f24a",
+    "url": "/imagenes/static/css/main.18e1671f.chunk.css"
+  },
+  {
+    "revision": "10025aaaa4df3a7c10fd",
+    "url": "/imagenes/static/js/2.f31bd1a4.chunk.js"
+  },
+  {
+    "revision": "24b203d241c93b80f24a",
+    "url": "/imagenes/static/js/main.50362fc1.chunk.js"
+  },
+  {
+    "revision": "52cc4a2a4b7c8eff6776",
+    "url": "/imagenes/static/js/runtime-main.50497200.js"
+  }
+]);
